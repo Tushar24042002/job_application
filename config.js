25d60799ba5e8e63650a0e1d21d71aaa673524b7
Tushar24042002 <sahilgupta24042002@gmail.com>
2024-07-02 16:59:25 +0530

    22 - june  commit

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
-import { Sequelize } from "sequelize";
+require('dotenv').config(); // Load environment variables from .env file
 
-const sequelize = new Sequelize('railway', 'root', 'RYqAfZRcoxbfqcGeosINSKINpTpGdfYN', {
-  host: 'monorail.proxy.rlwy.net',
-  port: 43489,
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
 });
 
-export default sequelize;
+module.exports = sequelize;
--- config/aws_credentials.js
+++ config/aws_credentials.js
@@ -1,7 +1,3 @@
-// export const aws_access_secret_key = "5CxvjpmDNKRTMAumNqBRmKchroUiS7G4QcoXrURp";
-// export const aws_access_key = "AKIA5DO2DQO63KSJHSSL";
-// export const aws_buecket_name = "tusharguptaawsbucket";
-
 
 export const aws_access_secret_key = process.env.aws_access_secret_key;
 export const aws_access_key = process.env.aws_access_key;
