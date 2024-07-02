4f68424497b2608a7f76bd12e7dc6a505232e440
Tushar24042002 <sahilgupta24042002@gmail.com>
2024-07-02 16:53:28 +0530

    22 - june  commit

--- config/aws_credentials.js
+++ config/aws_credentials.js
@@ -1,3 +1,8 @@
-export const aws_access_secret_key = "5CxvjpmDNKRTMAumNqBRmKchroUiS7G4QcoXrURp";
-export const aws_access_key = "AKIA5DO2DQO63KSJHSSL";
-export const aws_buecket_name = "tusharguptaawsbucket";
+// export const aws_access_secret_key = "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";
+// export const aws_access_key = "AKIA5DO2DQO63KSJHSSL";
+// export const aws_buecket_name = "tusharguptaawsbucket";
+
+
+export const aws_access_secret_key = process.env.aws_access_secret_key;
+export const aws_access_key = process.env.aws_access_key;
+export const aws_buecket_name = process.env.aws_buecket_name;
