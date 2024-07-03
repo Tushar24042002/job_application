4cb83ccfcda8e24b1aa8b25a1599e34098a6e013
Tushar24042002 <sahilgupta24042002@gmail.com>
2024-07-03 23:38:01 +0530

    first commit

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { Sequelize } from "sequelize";
-
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
