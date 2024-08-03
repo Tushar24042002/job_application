650d94d770a6383dd4d6c3441545f3628f25ea9e
Tushar24042002 <sahilgupta24042002@gmail.com>
2024-08-04 01:47:26 +0530

    4-8

--- config.js
+++ config.js
@@ -3,18 +3,10 @@ dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
-
-const sslKey = process.env.SSL_KEY.replace(/\\n/g, '\n');
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: process.env.DB_DIALECT,
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: true, 
-      ca: sslKey
-    },
-  },
 });
 
 export default sequelize;
