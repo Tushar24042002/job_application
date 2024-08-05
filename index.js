614f1c439ed684ed126696ec1ba6f133a9a4a889
Tushar Gupta <100976552+Tushar24042002@users.noreply.github.com>
2024-08-06 00:51:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import JobContoller from "./controllers/job.controller.js";
 import IndustryController from "./controllers/industry.controller.js";
 import JobSeekerController from "./controllers/jobSeeker.controller.js";
 import { chatHandler } from './chat/chat.js'; 
+import { chatHandler } from './chat/chat.js'; 
 import "./services/associationService.js";
 import cors from "cors";
 import { Server } from 'socket.io';
@@ -62,3 +63,46 @@ sequelize.sync({force : false}).then(() => {
 }).catch(error => {
     console.error('Unable to synchronize the database:', error);
 });
+
+
+
+import EmailTemplate from "./models/EmailTemplate.js";
+
+
+export const addEmail = async (subject, body) => {
+    try {
+        const data = await EmailTemplate.create({ subject, body });
+        console.log(data)
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError') {
+           console.log(error);
+        } else {
+           console.log(error);
+        }
+    }
+}
+
+await addEmail("Complete Your Registration - OTP Verification", `
+    <div style="font-family: Arial, sans-serif; line-height: 1.6;">
+      <h2>Complete Your Registration</h2>
+      <p>Hello, {userName},</p>
+      <p>Thank you for registering with our job portal. To complete your registration, please use the following One-Time Password (OTP) to verify your email address:</p>
+      <div style="font-size: 24px; font-weight: bold; text-align: center; margin: 20px 0; color: #333333;">{otp}</div>
+      <p>This OTP is valid for the next 10 minutes. Please do not share this OTP with anyone.</p>
+      <p>If you did not request this email, please ignore it.</p>
+      <p>Thank you,<br>Job Portal Team</p>
+    </div>
+`)
+
+
+await addEmail("Job Application Status Update",`
+    <div style="font-family: Arial, sans-serif; line-height: 1.6;">
+      <h2>Job Application Status Update</h2>
+      <p>Dear {userName},</p>
+      <p>We are writing to inform you that the status of your application for the position of <strong>{jobTitle}</strong> has been updated.</p>
+      <p><strong>Status Update:</strong> {statusMessage}</p>
+      <p>Thank you for your interest in joining our team. If you have any questions, please feel free to reach out to us.</p>
+      <p>Best regards,</p>
+      <p>The Hiring Team</p>
+    </div>
+  `)
